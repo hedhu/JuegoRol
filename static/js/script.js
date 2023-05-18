df658f92075eee474ae77a23520f7a433022ce14
hedhu <edvv.at@gmail.com>
2023-05-17 23:15:08 -0400

    conexionDB

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+function calcular() {
+    var num1 = parseInt(document.getElementById("num1"));
+    var num2 = parseInt(document.getElementById("num2"));
+    var resultados = document.getElementById("resultado");
+
+    resultados = 
+
+}
