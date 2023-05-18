8a04e7ed96857309340da266abf31bee08865c85
hedhu <edvv.at@gmail.com>
2023-05-18 17:49:17 -0400

    op

--- app.py
+++ app.py
@@ -6,15 +6,23 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'abc.123'
 app.config['MYSQL_DB'] = 'juegorol'
-
-
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route("/usuario/<nombre>")
+def paginaUsuario():
+    conn = mysql.connection
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM juegorol.usuarios")
+    usuarios = cursor.fetchall()
+    cursor.close()
+    return render_template("usuario.html")
+
 
 @app.route('/usuarios')
 def mostrar_usuarios():
@@ -22,8 +30,9 @@ def mostrar_usuarios():
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM juegorol.usuarios")
     usuarios = cursor.fetchall()
+    nusuarios = len(usuarios)
     cursor.close()
-    return render_template('usuarios.html', usuarios=usuarios)
+    return render_template('usuarios.html', usuarios=usuarios, nusuarios=nusuarios)
 
 @app.route('/personajes')
 def mostrar_personajes():
@@ -36,5 +45,6 @@ def mostrar_personajes():
     return render_template('personajes.html', pj=pj)
 
 mysql = MySQL(app)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug= True)
